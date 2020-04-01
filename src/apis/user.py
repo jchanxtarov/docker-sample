5a17f3477af09ab8259da1e3d898a4da478beb93
jchanxtarov <shi3mizu14happy@gnail.com>
2020-04-02 00:23:44 +0900

    [fix] format

--- src/apis/user.py
+++ src/apis/user.py
@@ -4,25 +4,25 @@ from src.models.user import UserModel, UserSchema
 from src.database import db
 
 class UserAPI(Resource):
-  def __init__(self):
-    self.reqparse = reqparse.RequestParser()
-    self.reqparse.add_argument('name', required=True)
-    super(UserAPI, self).__init__()
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', required=True)
+        super(UserAPI, self).__init__()
 
-  def getUserList(self):
-    results = UserModel.query.all() # consider: これで取れる仕組みが理解できていない
-    user_schema = UserSchema(many=True).dump(results).data
-    users = user_schema.dump(results).data
+    def getUserList(self):
+        results = UserModel.query.all() # consider: これで取れる仕組みが理解できていない
+        user_schema = UserSchema(many=True).dump(results).data
+        users = user_schema.dump(results).data
 
-    return users
+        return users
 
-  def postUser(self):
-    print("[test] hogehogehogehoge")
-    args = self.reqparse.parse_args()
-    user = UserModel(
-      name = args.name
-    )
-    db.session.add(user)
-    db.session.commit()
+    def postUser(self):
+        print("[test] hogehogehogehoge")
+        args = self.reqparse.parse_args()
+        user = UserModel(
+            name=args.name
+        )
+        db.session.add(user)
+        db.session.commit()
 
-    return user
+        return user
--- src/views/user.py
+++ src/views/user.py
@@ -8,24 +8,21 @@ user_router = Blueprint('user_router', __name__)
 
 @user_router.route('/', methods=['GET'])
 def getUserList():
-
-  users = UserAPI.getUserList()
-  user_schema = UserSchema(many=True)
-
-  return make_response(jsonify({
-    'code': 200,
-    'users': users
-  }))
+    users = UserAPI.getUserList()
+    
+    return make_response(jsonify({
+        'code': 200,
+        'users': users
+    }))
 
 @user_router.route('/', methods=['POST'])
 def registUser():
+    # jsonデータを取得する
+    jsonData = json.dumps(request.json)
+    userData = json.loads(jsonData)
+    user = UserAPI.postUser(userData)
 
-  # jsonデータを取得する
-  jsonData = json.dumps(request.json)
-  userData = json.loads(jsonData)
-  user = UserAPI.postUser(userData)
-
-  return make_response(jsonify({
-    'code': 200,
-    'user': user
-  }))
+    return make_response(jsonify({
+        'code': 200,
+        'user': user
+    }))
